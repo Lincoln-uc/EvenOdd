051b17cd14db837f91ef3b5a7a30bfa9d9e8e9bb
Lincoln-uc <linctorhake@gmail.com>
2024-09-23 20:23:07 -0400

    EvenOdd

---
+++ src/EvenOdd.java
@@ -0,0 +1,8 @@
+public class EvenOdd {
+    public static void main(String[] args) {
+        System.out.println("Input number below to see if it is even or odd");
+        int numToExamine = 2;
+// program will then determine that since 2 mod 2 is 0 that numToExamine is Even.
+        System.out.println("The number you entered is even");
+    }
+}
